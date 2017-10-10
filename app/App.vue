a7a7ee5d774921231f7fe9a172837843b01fb79a
Viktor Babiy <v.babiy@viseven.com>
2017-10-10 21:46:09 +0300

    add(*): electron + vue template

---
+++ app/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ test }}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                test: 'Hello world!'
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ExternalsPlugin('commonjs', [
+            'electron'
+        ])
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        }
+    }
+};
