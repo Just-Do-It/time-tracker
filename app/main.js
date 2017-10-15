03b966d391e9345248b799a8c90c9f36e6ce7423
victorAA <victor.a.babiy@gmail.com>
2017-10-16 00:05:17 +0300

    update(AuthService): in-progress

--- app/main.js
+++ app/main.js
@@ -3,8 +3,12 @@ import store from './store'
 import router from './router'
 import Vuetify from 'vuetify'
 import App from './App.vue'
+import AuthService from './services/AuthService'
 
 Vue.use(Vuetify)
+
+// todo remove
+window.auth = AuthService
 
 /* eslint-disable no-new */
 new Vue({
