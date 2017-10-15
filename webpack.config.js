534cc655a6e972281978b44b825a41de13789da8
FEDQK <vovand1996@gmail.com>
2017-10-15 18:31:04 +0300

    add(webpack.config): css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,11 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },
+            {
+               test: /\.css$/,
+               exclude: /node_modules/,
+               loader: 'css-loader'
             }
         ]
     },
