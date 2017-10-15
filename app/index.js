c4adef5f6589d284c7e774438efb9887114a0b5a
victorAA <victor.a.babiy@gmail.com>
2017-10-15 18:51:17 +0300

    fix(app/index.js): router

--- app/index.js
+++ app/index.js
@@ -1,19 +1,11 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import Vuetify from 'vuetify';
 import Vuex from 'vuex';
-import routes from './routes';
+import router from './router';
 import App from './App.vue';
 
-Vue.use(VueRouter);
 Vue.use(Vuetify);
 Vue.use(Vuex);
-
-const router = new VueRouter({
-    routes,
-    linkActiveClass: 'active',
-    linkExactActiveClass: 'active'
-});
 
 new Vue({
     router,
