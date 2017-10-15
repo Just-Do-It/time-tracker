4ca054017e6893712bf5c38be0a3024db5f26eb2
victorAA <victor.a.babiy@gmail.com>
2017-10-15 20:20:25 +0300

    refactor(*): rename index.js to main.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './app/index.js',
+  entry: './app/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
