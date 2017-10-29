d42742bd78ab0f8a91a477e79b25814da103a468
FEDQK <vovand1996@gmail.com>
2017-10-29 17:21:32 +0200

    add(Subtask): button delete

--- app/components/TimeTracker/Subtask.vue
+++ app/components/TimeTracker/Subtask.vue
@@ -1,21 +1,17 @@
 <template>
   <v-container class="container_subtask">
     <v-layout justify-end>
-      <v-flex xs10>
+      <v-flex xs11>
         <v-list>
-          <v-list-tile
-            v-for="subtask in taskData.subTasks"
-            :key="subtask.id"
-          >
+          <v-list-tile v-for="subtask in taskData.subTasks" :key="subtask.id">
             <v-layout align-center>
               <v-icon>subdirectory_arrow_right</v-icon>
-              <!-- <v-checkbox hide-details :label="subtask.name" v-model="subtask.status"></v-checkbox>
-              <v-btn icon @click="openDialogEdit(subtask)"><v-icon>mode_edit</v-icon></v-btn> -->
               <template v-if="idSubtask == subtask.id">
                 <v-text-field
                   name="subtask-name"
                   label="Subtask name"
                   v-model="subtask.name"
+                  autofocus
                   hide-details
                 ></v-text-field>
                 <v-btn class="success" @click="editSubtask">OK</v-btn>
@@ -23,6 +19,7 @@
               <template v-else>
                 <v-checkbox hide-details :label="subtask.name" v-model="subtask.status"></v-checkbox>
                 <v-btn icon @click="editSubtask(subtask.id)"><v-icon>mode_edit</v-icon></v-btn>
+                <v-btn icon @click="deleteSubtask(subtask.id)"><v-icon>delete</v-icon></v-btn>
               </template>
             </v-layout>
           </v-list-tile>
@@ -33,13 +30,10 @@
       <v-flex xs1>
         <v-layout><v-icon>subdirectory_arrow_right</v-icon></v-layout>
       </v-flex>
-      <v-flex xs9>
+      <v-flex xs10>
         <v-btn block @click="createSubtask">Add subtask</v-btn>
       </v-flex>
     </v-layout>
-    <!-- <v-dialog v-model="idSubtask">
-      <edit-subtask :taskData="taskData" :idSubtask="idSubtask"></edit-subtask>
-    </v-dialog> -->
   </v-container>
 </template>
 
@@ -47,10 +41,7 @@
   export default{
     data() {
       return {
-        idSubtask: null,
-        editInfo: {
-          currentEditSubtask: null
-        },
+        idSubtask: null
       }
     },
     props: {
@@ -58,27 +49,38 @@
     },
     methods: {
       createSubtask() {
-        let lastId = this.taskData.subTasks[this.taskData.subTasks.length - 1].id
+        let lastId = this.taskData.subTasks.length > 0 ? this.taskData.subTasks[this.taskData.subTasks.length - 1].id : 0
         let newSubtask = {
           id: lastId + 1,
-          name: "test",
+          name: "subtask name",
           status: false
         }
         this.taskData.subTasks.push(newSubtask)
       },
+      deleteSubtask(id) {
+        this.taskData.subTasks.some((element, index, array) => {
+          if(element.id === id) {
+            this.taskData.subTasks.splice(index, 1)
+            return element
+          }
+        })
+      },
       editSubtask(id) {
         this.idSubtask = id || null
-        // this.currentEditSubtask = currentSubtask;
       }
     }
   }
 </script>
 
-<style scoped>
+<style>
   .container_subtask {
     padding-top: 0;
   }
-  .container_subtask ul {
+  .application--light .list {
     background: transparent;
+  }
+  .input-group--active label {
+    font-style: italic;
+    text-decoration: line-through;
   }
 </style>
