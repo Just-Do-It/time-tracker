c4c4ef55bef9ba5aad2b4d557e718974dee0b896
victorAA <victor.a.babiy@gmail.com>
2017-10-31 22:24:29 +0200

    update(router): fix electorn start, remove history mode

--- app/router/index.js
+++ app/router/index.js
@@ -3,13 +3,11 @@ import Router from 'vue-router'
 import AuthGuard from './auth-guard'
 
 import SignUp from '../components/Auth/SignUp.vue'
-import SignIn from '../components/Auth/SignIn.vue'
 import TimeTracker from '../components/TimeTracker/TimeTracker.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
   linkActiveClass: 'active',
   linkExactActiveClass: 'active',
   routes: [
